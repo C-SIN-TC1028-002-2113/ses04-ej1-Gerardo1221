761aee153f8c5f1c3fe5f67337328f05ca6e4f1a
Gerardo1221 <a01740899@itesm.mx>
2021-08-20 15:37:28 +0000

    trabajos

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,6 +1,18 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
+    edad = int(input("Ingresa tu edad: "))
+    if edad<=0 or edad>120: 
+        print("Respuesta incorrecta")
+    elif edad<18:
+        print("No cumples requisitos")
+    elif edad>=18:
+        Id = input("¿Tienes identificación oficial? (s/n): ") 
+        if Id== "s":
+            print("Trámite de licencia concedido")
+        elif Id== "n":
+            print("No cumples requisitos")
+        else:
+            print("Respuesta incorrecta")
+    
 if __name__ == '__main__':
     main()
